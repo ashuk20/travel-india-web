02dbfccf09077fc4645545aa84ccb227b986f580
ashuk20 <ashwinikhodake7@gmail.com>
2026-07-27 20:09:22 +0530

    chore: setup Supabase and project foundation

---
+++ lib/core/config/app_config.dart
@@ -0,0 +1,8 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class AppConfig {
+  static const appName = "Travel India";
+  static const supabaseUrl = "https://zlssevxvvfxswnyocnhv.supabase.co";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inpsc3Nldnh2dmZ4c3dueW9jbmh2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxNTM0NDYsImV4cCI6MjEwMDcyOTQ0Nn0.xQt-2ZoG0-AZNOcrbnahop-_xlj9u---YdYfjUdurqs";
+}
